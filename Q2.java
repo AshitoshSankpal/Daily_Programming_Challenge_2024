33b0479ac1bc1a7a5c30ff4f5d7a49492adc90af
unknown <ashitoshsankpal7@gmail.com>
2024-09-15 19:11:07 +0530

    added Q2

--- Q2.java
+++ Q2.java
@@ -1,1 +1,27 @@
-class Q2{}
+class Q2{
+    
+    
+        
+        public static int findMissingNumber(int[] arr, int n) {
+            
+            int totalSum = n * (n + 1) / 2;
+            
+            
+            int arrSum = 0;
+            for (int num : arr) {
+                arrSum += num;
+            }
+            
+           
+            return totalSum - arrSum;
+        }
+    
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 4, 5}; 
+            int n = arr.length + 1; 
+            
+            int missingNumber = findMissingNumber(arr, n);
+            System.out.println("Missing Number: " + missingNumber);
+        }
+    }
+    
